8588b9d68814b94456030c392adbb352145afbc3
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-01-05 18:52:52 +0700

    Add new file for firs LAB (SearchnSort)

---
+++ VSCode/SortAndSearch/Main.java
@@ -0,0 +1,18 @@
+package LAB211.SortAndSearch;
+
+import java.util.*;
+
+import LAB211.SortAndSearch.Model.Model;
+import LAB211.SortAndSearch.View.*;
+import LAB211.SortAndSearch.Controller.*;
+
+public class Main {
+
+    // public Main () {
+
+    // }
+    public static void main(String[] args) {
+        Utilities util = new Utilities();
+        util.runner();
+    }
+}
---
+++ VSCode/SortAndSearch/Model/Model.java
@@ -0,0 +1,31 @@
+package LAB211.SortAndSearch.Model;
+
+import java.util.*;
+
+public class Model {
+    //private int size;
+    private int[] array;
+
+    public Model() {
+    }
+
+    public Model(int[] array) {
+        this.array = array;
+    }
+
+    // public int getSize() {
+    //     return size;
+    // }
+
+    // public void setSize(int size) {
+    //     this.size = size;
+    // }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int array[]) {
+        this.array = array;
+    }
+}
