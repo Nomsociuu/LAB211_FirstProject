d3faafeac77a77c913d015a4eea5f5783eecbef5
Huynh Dinh Thien <136036261+Nomsociuu@users.noreply.github.com>
2024-01-05 07:22:02 +0700

    okay

---
+++ Sort/src/Model/Model.java
@@ -0,0 +1,35 @@
+package Model;
+
+import java.util.Random;
+
+public class Model {
+
+    private int size;
+    private int value;
+
+    public Model() {
+    }
+
+    public Model(int size, int value) {
+        this.size = size;
+        this.value = value;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    
+}
