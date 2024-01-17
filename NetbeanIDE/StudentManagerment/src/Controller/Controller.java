c8a69e7a4a968944ad0953c18b978b2ab21615cf
Nomsociuu <nomsociuu2004@gmail.com>
2024-01-17 10:11:28 +0700

    Đại tướng raw

--- NetbeanIDE/StudentManagerment/src/Controller/Controller.java
+++ NetbeanIDE/StudentManagerment/src/Controller/Controller.java
@@ -1,42 +1,92 @@
 package Controller;
 
-
 import Model.Student;
-import View.Menu;
-import java.util.ArrayList;
+import java.util.*;
 
-public class Controller {
 
-    private ArrayList<Student> std = new ArrayList<>();
-    Menu menu = new Menu();
+public class Controller extends ArrayList<Student>{
 
-    public void run() {
+    final Scanner sc = new Scanner(System.in);
+    final List<Student> students = new ArrayList<Student>();
+
+    public void CreateStudent(ArrayList<Student> students) {
+        String name, semeter, id, course;
+        int count = 0;
+        // while (n <= 10){
+        //     System.out.print("Enter student name: ");
+        //     name = sc.nextLine();
+        //     System.out.print("Enter student course: ");
+        //     course = sc.nextLine();
+        //     Model st = new Model(name, course);
+        //     System.out.println("Student " + "" + " had been added");
+        //     boolean continueornot;
+        //     System.out.println("Do you want to continue?: ");
+        //     continueornot = continueornot(sc.nextLine());
+        // }
+
         while (true) {
-            int choice;
-            do {
-                menu.displayMainMenu();
-                choice = menu.getUserChoice(1, 5);
+            System.out.println("Enter student id: ");
+            id = sc.nextLine();
+            System.out.println("Enter student name: ");
+            name = sc.nextLine();
+            System.out.println("Enter semeter: ");
+            semeter = sc.nextLine();
+            System.out.println("Enter course: ");
+            course = sc.nextLine();
+            Student st = new Student(name, semeter, course, id);
+            students.add(st);
+        }
 
-                switch (choice) {
-                    case 1:
-                        System.out.println("Create");
-                        break;
-                    case 2:
-                        System.out.println("Find and Sort");
-                        break;
-                    case 3:
-                        System.out.println("Update/Delete");
-                        break;
-                    case 4:
-                        System.out.println("Report");
-                        break;
-                    case 5:
-                        System.out.println("Exiting program. Goodbye!");
-                        System.exit(0);
-                }
-            } while (choice > 0 && choice <= 5);
+        // if (count > 10) {
+        //     System.out.println("Continue? (Y/N): ");
+        //     if (continueornot(sc.nextLine())){
+        //         return;
+        //     }
+        // }
 
+    }
+
+    public boolean continueornot(String yesorno){
+        while (true){
+            if (yesorno.equalsIgnoreCase("Y")){
+                return true;
+            }   
+            else if (yesorno.equalsIgnoreCase("N")){
+                return false;
+            }
         }
     }
 
+    // public void findandsort(ArrayList<Student> students) {
+    //     ArrayList<Student> list = new ArrayList<>(students);
+    //     Collections.sort((List<T>) list);
+    //     for (Student student : list) {
+    //         System.out.println(student);
+    //     }
+    // }
+
+    public static void BubbleSort(int array[]) {
+        int n = array.length;
+        boolean swap;
+
+        for (int i = 0; i < n - 1; i++) {
+            swap = false;
+            // System.out.println("\nCheck value " + (i + 1) + ":");
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                System.out.println("Array was sorted using Bubble sort!");
+                System.out.println(Arrays.toString(array));
+                //System.out.println("All array value is sorted!");
+                break;
+            }
+        }
+    }
 }
