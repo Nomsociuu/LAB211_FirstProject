c6d0df76e7ec93333c8f20d51923f5edb157eec2
kleqing <78801337+kleqing@users.noreply.github.com>
2024-01-26 07:53:11 +0700

    Another for VSCode

---
+++ VSCode/Calculate/Main.java
@@ -0,0 +1,10 @@
+package LAB211.Calculate;
+
+import LAB211.Calculate.Controller.*;
+
+public class Main {
+    public static void main(String[] args){
+        Control controller = new Control();  
+        controller.run();
+    }  
+}
