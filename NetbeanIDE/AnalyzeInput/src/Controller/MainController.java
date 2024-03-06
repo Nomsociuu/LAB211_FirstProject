59c22b6c779f70a9f605e0af6dc92f0fd7014b66
Nomsociuu <nomsociuu2004@gmail.com>
2024-03-06 10:29:17 +0700

    Ana-NọnMVC

---
+++ NetbeanIDE/AnalyzeInput/src/Controller/MainController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import View.Menu;
+import Common.Algo;
+//import Model.x;
+
+public class MainController extends Menu {
+
+    Algo algo;
+
+    public MainController() {
+        super("===== Analysis String program ====", new String[]{"Analysis",
+            "Exit"});
+        algo = new Algo();
+    }
+
+    @Override
+    public void execute(int n) {
+        switch (n) {
+            case 1 ->
+                algo.Analize();
+            case 2 ->
+                System.exit(0);
+        }
+    }
+
+}
